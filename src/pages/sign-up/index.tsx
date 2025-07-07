23a2b89263eaaa616a4ee4dfa151fe210ee6b8c0
LoFrance <lorenzo.franceschini@pagopa.it>
2025-07-07 10:48:38 +0200

    Add raw signup form

--- src/pages/sign-up/index.tsx
+++ src/pages/sign-up/index.tsx
@@ -2,6 +2,9 @@ export const SignUp = () => {
   return (
     <>
       <h1>Sign Up</h1>
+      <label for="email">Email</label>
+      <input id="email" type="email" />
+      <button disabled>Sign Up</button>
     </>
   );
 };
